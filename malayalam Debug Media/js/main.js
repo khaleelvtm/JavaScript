5ad75820f484d1f57ae03fe809739835e7eb75ef
khaleel rahman <44889466+khaleelvtm@users.noreply.github.com>
2023-02-09 12:59:52 +0530

    json

--- malayalam Debug Media/js/main.js
+++ malayalam Debug Media/js/main.js
@@ -5,3 +5,20 @@ let myObject = {
 };
 
 console.log(myObject);
+console.log(typeof myObject);
+
+let myJSON = `{
+    "id": 1,
+    "name": "Smith",
+    "company": "Google"
+}`;
+
+console.log(myJSON);
+
+const parsedJSON = JSON.parse(myJSON);
+console.log(parsedJSON);
+console.log(typeof parsedJSON);
+
+const stringyJSON = JSON.stringify(myObject);
+console.log(stringyJSON);
+console.log(typeof stringyJSON);
