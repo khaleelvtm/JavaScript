6deed6ce0c4164a9694708075317ca15623abaab
khaleel rahman <44889466+khaleelvtm@users.noreply.github.com>
2023-02-24 12:12:31 +0530

    template

---
+++ code-malayalam/js/24-template.js
@@ -0,0 +1,39 @@
+// document.body.innerHTML = `
+//     <div>
+//         <h1>Heading</h1>
+//         <p>Text : 677</p>
+//     </div>
+// `;
+let userName = "John";
+let age = 26;
+
+let value = `The ${userName} age is ${age}`;
+console.log(value);
+
+let aveAge = `The age is ${age > 25 ? "High" : "Low"}`;
+console.log(aveAge);
+
+// ------------------------
+
+const product = [
+	{
+		productName: "Pen",
+		price: 25,
+	},
+	{
+		productName: "Book",
+		price: 40,
+	},
+	{
+		productName: "NoteBook",
+		price: 32,
+	},
+];
+
+document.body.innerHTML = product.map((item) => {
+	return `
+    <div>
+        <h1>Product Name: ${item.productName}</h1>
+        <p>Product Price: ${item.price}</p>
+    </div>`;
+});
