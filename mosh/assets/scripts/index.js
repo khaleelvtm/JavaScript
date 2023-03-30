7ab45b7432927bc6276e5110c0c2d3da618bc491
khaleel rahman <44889466+khaleelvtm@users.noreply.github.com>
2023-03-30 22:25:30 +0530

    03-object-oriented-two

--- mosh/assets/scripts/index.js
+++ mosh/assets/scripts/index.js
@@ -1,20 +1,44 @@
 // -----
-function createCircle(radius) {
-	return {
-		radius,
-		draw: function () {
-			console.log("draw");
-		},
-	};
-}
-const circle = createCircle(1);
+// 	return {
+// function createCircle(radius) {
+// 		radius,
+// 		draw: function () {
+// 			console.log("draw");
+// 		},
+// 	};
+// }
+// const circle = createCircle(1);
 
 function Circle(radius) {
-	console.log("THIS: ", this);
 	this.radius = radius;
-	this.draw = function () {
-		console.log("Draw");
+
+	this.defaultLocation = {
+		x: 0,
+		y: 0,
 	};
+
+	this.computeOptimLocation = function (fact) {
+		//.....
+	};
+
+	this.getDefaultLocation = function () {
+		return this.defaultLocation;
+	};
+
+	this.draw = function () {
+		console.log("Draw Start");
+	};
+
+	Object.defineProperty(this, "defaultLocation", {
+		get: function () {
+			return defaultLocation;
+		},
+		set: function (value) {
+			if (!value.x || !value.y) throw new Error("Invalid Locarion ! 404");
+			defaultLocation = value;
+		},
+	});
 }
 
-const anothOne = new Circle(1);
+const circle = new Circle(1);
+circle.defaultLocation = 1;
