9562ba651da4aff069693b14d01a4bfa5b5abefc
khaleel rahman <44889466+khaleelvtm@users.noreply.github.com>
2023-04-04 09:58:52 +0530

    math

--- DaveGray/assets/scripts/index.js
+++ DaveGray/assets/scripts/index.js
@@ -1,2 +1,16 @@
 let myVariable = "Javascript";
 console.log(myVariable.toUpperCase());
+
+let myNumber = 55.9;
+let myString = "55";
+
+console.log(myString);
+console.log(typeof Number.parseFloat("45.678ac").toFixed(1).toString());
+
+// Math
+console.log(Math.PI);
+console.log(Math.trunc(Math.PI));
+
+// log
+let name = "wilsmith";
+console.log(name.charAt(Math.floor(Math.random() * name.length)));
