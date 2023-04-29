4e000584a05ad1549f8ab4548ac4d181ba853110
khaleel rahman <44889466+khaleelvtm@users.noreply.github.com>
2023-04-29 21:39:43 +0530

    'start'

---
+++ ej/script.js
@@ -0,0 +1,30 @@
+/*
+function theLocation() {
+    let city = "San Francisco";
+
+    return {
+        get: function () {
+            console.log(city);
+        },
+        set: function (newCity) {
+            city = newCity;
+        },
+    };
+}
+
+let newPlace = theLocation();
+newPlace.get();
+newPlace.set("Berlin");
+newPlace.get();
+// Video - 
+*/
+a = 5;
+b = 12;
+let number = a + b;
+
+console.log(number);
+
+while (number < 25) {
+    console.log(number);
+    number++;
+}
