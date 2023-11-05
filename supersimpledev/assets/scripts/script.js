60c7993c6e88fa6d46b84628de4ee638e8b4ad84
khaleel rahman <44889466+khaleelvtm@users.noreply.github.com>
2023-11-05 15:41:53 +0530

    [NINJA] part two timeout animation done

--- supersimpledev/assets/scripts/script.js
+++ supersimpledev/assets/scripts/script.js
@@ -1,63 +1,66 @@
-// Canvas
+// Animated countdown
+
+//Update Time
+function updateTimer(deadline) {
+    let time = deadline - new Date();
+
+    return {
+        days: Math.floor(time / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((time / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((time / 1000 / 60) % 60),
+        seconds: Math.floor((time / 1000) % 60),
+        total: time,
+    };
+}
+
+// Animated Span
+function animateClock(span) {
+    span.className = "turn";
+    setTimeout(function () {
+        span.className = "";
+    }, 700);
+}
+
+// Start Timer
+function startTimer(id, deadline) {
+    let timeIntervel = setInterval(function () {
+        let clock = document.getElementById(id);
+        let timer = updateTimer(deadline);
+
+        clock.innerHTML = `
+            <span>${timer.days}</span>
+            <span>${timer.hours}</span>
+            <span>${timer.minutes}</span>
+            <span>${timer.seconds}</span>
+        `;
+
+        // Animation Span
+        let spans = clock.getElementsByTagName("span");
+        animateClock(spans[3]);
+        if (timer.seconds == 59) {
+            animateClock(spans[2]);
+        } else if (timer.minutes == 59 && timer.seconds == 59) {
+            animateClock([spans[1]]);
+        } else if (
+            timer.hours == 23 &&
+            timer.minutes == 59 &&
+            timer.seconds == 59
+        ) {
+            animateClock(spans[0]);
+        }
+
+        // Time End Check
+        if (timer.total < 1) {
+            clearInterval(timeIntervel);
+            clock.innerHTML = `<span>0</span><span>0</span><span>0</span><span>0</span>`;
+        }
+    }, 1000);
+}
+
+// Set Time
 window.onload = function () {
-    let canvas = document.getElementById("sky");
-    let ctx = canvas.getContext("2d");
-
-    console.log(canvas);
-    // Width and Height
-    let canWidth = window.innerWidth;
-    let canHeight = window.innerHeight;
-    canvas.width = canWidth;
-    canvas.height = canHeight;
-
-    // Generate Snow
-    let maxFlakes = 100;
-    let flakes = [];
-
-    // Loop Flakes
-    for (let i = 0; i < maxFlakes; i++) {
-        flakes.push({
-            x: Math.random() * canWidth,
-            y: Math.random() * canHeight,
-            r: Math.random() * 5 + 2,
-            d: Math.random() + 1,
-        });
-    }
-
-    // Draw Flakes
-    function drawFlakes() {
-        ctx.clearRect(0, 0, canWidth, canHeight);
-        ctx.fillStyle = "white";
-        ctx.beginPath();
-
-        for (let i = 0; i < maxFlakes; i++) {
-            let flak = flakes[i];
-            ctx.moveTo(flak.x, flak.y);
-            ctx.arc(flak.x, flak.y, flak.r, 0, Math.PI * 2, true);
-        }
-
-        ctx.fill();
-        moveFlake();
-    }
-
-    // Animate Move Flake
-    let angle = 0;
-    function moveFlake() {
-        angle += 0.01;
-        for (let i = 0; i < maxFlakes; i++) {
-            let flak = flakes[i];
-            flak.y += Math.pow(flak.d, 2) + 1;
-            flak.x += Math.sin(angle) * 2;
-
-            if (flak.y > canHeight) {
-                flakes[i] = {
-                    x: Math.random() * canWidth,
-                    y: 0,
-                    r: flak.r,
-                    d: flak.d,
-                };
-            }
-        }
-    }
-    setInterval(drawFlakes, 25);
+    let deadline = new Date("November 5, 2023 15:37:00");
+    startTimer("clock", deadline);
 };
+
+console.log(updateTimer(new Date("April 25, 2024 15:15:00")));
