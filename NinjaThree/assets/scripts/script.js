6b6136bf9037c11889e47e3922d6564cfc83a9f8
khaleel rahman <44889466+khaleelvtm@users.noreply.github.com>
2023-11-13 06:42:35 +0530

    [JS] ninja DOM finished

--- NinjaThree/assets/scripts/script.js
+++ NinjaThree/assets/scripts/script.js
@@ -1,122 +1,77 @@
-// JS ES6
-/*
-let list = document.getElementsByTagName("li");
+// JS DOM
 
-for (let i = 0; i < list.length; i++) {
-    list[i].onclick = function () {
-        console.log(i);
-    };
-}
+let list = document.querySelector("ul");
+list.addEventListener("click", function (e) {
+    if (e.target.className === "delete") {
+        let li = e.target.parentElement;
+        li.remove();
+    }
+});
 
-function number(num = 23) {
-    console.log(num);
-}
-number(1);
+let addForm = document.forms["add-book"];
+addForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let value = addForm.querySelector("#textInput");
 
-function logUser(name = "Mike", dress = "Red", age = 25) {
-    console.log(
-        "My name is " +
-            name +
-            " and my dress color is " +
-            dress +
-            " and my age is " +
-            age
-    );
-}
-logUser("John Smith", "White", 32);
+    let li = document.createElement("li");
+    let bookName = document.createElement("span");
+    let deleteBtn = document.createElement("span");
 
-let myArry = ["one", "two", "three"];
-console.log(...myArry);
+    bookName.classList.add("name");
+    deleteBtn.classList.add("delete");
 
-let num1 = [1, 2, 3, 4, 5];
-let num2 = [...num1, 6, 7, 8, 9, 10];
-console.log(num2);
+    bookName.innerHTML = value.value;
+    deleteBtn.innerHTML = "Delete";
 
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
 
-let number = [4, 6, 8];
-function calcNum(a, b, c) {
-    console.log(a + b + c);
-}
-calcNum(...number);
+    value.value = "";
+});
 
-let user = "John Smith";
-let color = "Red";
-let age = 25;
+let hideAll = document.querySelector("#hideAll");
+hideAll.addEventListener("change", function () {
+    if (hideAll.checked) {
+        list.style.display = "none";
+    } else {
+        list.style.display = "flex";
+    }
+});
 
-let myObgect = {
-    user,
-    color,
-    age,
-    fun(x) {
-        console.log(`My number is ${x}`);
-    },
-};
-console.log(myObgect.fun(44));
+let search = document.forms["search-books"].querySelector("input");
+search.addEventListener("keyup", function (e) {
+    let term = e.target.value.toLowerCase();
+    let books = list.getElementsByTagName("li");
 
+    Array.from(books).forEach(function (book) {
+        let title = book.firstElementChild.textContent;
 
-let myVal = "Helooo ";
-console.log(myVal.repeat(10));
+        if (title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = "flex";
+        } else {
+            book.style.display = "none";
+        }
+    });
+});
 
-let input = document.getElementsByTagName("input");
-let val = input[0].value;
+// Tabs
+let tabs = document.querySelector(".tabs");
+let panels = document.querySelectorAll(".panel");
 
-console.log(myVal.startsWith(val));
+tabs.addEventListener("click", function (e) {
+    if (e.target.tagName == "LI") {
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.tagName);
 
-let youSay = "Goodbye";
-if (youSay.startsWith("Goodbye")) {
-    let iSay = "Hello";
-    console.log(`You say is ${youSay}, and i say ${iSay}`);
-}
-
-let msg = "Hi, welcome to my world";
-console.log(msg.includes("my"));
-
-
-const myFun = (name, age) =>
-    console.log(`My name is ${name} and my age is ${age }`);
-myFun("Smith", 23);
-
-let myObgect = {
-    name: "John Smith",
-    myFun(x) {
-        window.setInterval(() => {
-            if (x > 0) {
-                console.log(this.name + " is my best freind");
-                x--;
+        let targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function (panel) {
+            if (panel == targetPanel) {
+                panel.classList.add("active");
+            } else {
+                panel.classList.remove("active");
             }
-        }, 1000); 
-    },
-};
-myObgect.myFun(5);
-
-
-let newSet = new Set();
-newSet.add("John").add("Smith").add("Wills").add("John");
-newSet.delete("John");
-//newSet.clear();
-console.log(newSet.has("Smith"));
-
-console.log(newSet.size);
-
-console.log(newSet);
-
-let names = ["John", "Wils", "Doe", "Adome", "John"];
-
-let nameSet = new Set(names);
-
-console.log(names);
-console.log(nameSet);
-
-names = [...nameSet];
-console.log(names);
-*/
-
-function* gen() {
-    yield "One";
-    yield console.log("Two");
-    console.log("Three");
-}
-let myGen = gen();
-console.log(myGen.next());
-myGen.next();
-myGen.next();
+        });
+    }
+});
