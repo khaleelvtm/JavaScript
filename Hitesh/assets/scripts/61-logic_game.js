9bcd13a158194b6154ae10eb9d1ae3d2e80b0020
khaleel rahman <44889466+khaleelvtm@users.noreply.github.com>
2024-02-29 23:51:05 +0530

    [logic game] javascript completed

---
+++ Hitesh/assets/scripts/61-logic_game.js
@@ -0,0 +1,54 @@
+// EP: 61 / 65 - Logic Game
+let cards = document.querySelectorAll(".card");
+let isFlipped = false;
+let firstCard;
+let secondCard;
+
+cards.forEach((card) => card.addEventListener("click", flip));
+
+function flip() {
+    this.classList.add("flip");
+    if (!isFlipped) {
+        isFlipped = true;
+        firstCard = this;
+    } else {
+        secondCard = this;
+        checkIt();
+    }
+}
+
+function checkIt() {
+    if (firstCard.dataset.image === secondCard.dataset.image) {
+        success();
+    } else {
+        fail();
+    }
+}
+
+function success() {
+    firstCard.removeEventListener("click", flip);
+    secondCard.removeEventListener("click", flip);
+    reset();
+}
+
+function fail() {
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        reset();
+    }, 1000);
+}
+
+function reset() {
+    isFlipped = false;
+    firstCard = null;
+    secondCard = null;
+}
+
+(function shuffle() {
+    cards.forEach((card) => {
+        let index = Math.floor(Math.random() * 16);
+        card.style.order = index;
+        console.log(index);
+    });
+})();
--- Hitesh/assets/scripts/script.js
+++ Hitesh/assets/scripts/script.js
@@ -190,7 +190,6 @@ let employee = {
 for (const key in employee) {
     console.log(key, ":", employee[key]);
 }
-*/
 
 // EP: 31 / 65
 let user = {
@@ -208,3 +207,358 @@ let user = {
 };
 
 user.getCourse();
+
+// EP: 35 / 65
+let timer = document.querySelector(".timer");
+let followers = document.querySelector(".followers");
+let count = 0;
+
+setInterval(() => {
+    if (count < 1000) {
+        count++;
+        timer.innerHTML = count;
+    } else if (count == 1000) {
+        followers.innerHTML = `${count} Instagram Followers`;
+    }
+}, 1);
+
+setTimeout(() => {
+    followers.innerHTML = `${count} Instagram Followers`;
+}, 3000);
+
+// EP: 36 / 65
+let red = document.querySelector(".red");
+console.log(window.getComputedStyle(red).backgroundColor);
+
+const getBGBackground = (selectedElment) => {
+    return window.getComputedStyle(selectedElment).backgroundColor;
+};
+console.log("BG COLOR:", getBGBackground(red));
+
+// EP: 37 / 65
+red.addEventListener("mouseenter", () => {
+    console.log("MOUSE");
+    red.style.backgroundColor = "green";
+});
+// EP: 38 / 65
+const Users = function (firstName, dateOfBirth, location) {
+    this.firstName = firstName;
+    this.dateOfBirth = dateOfBirth;
+    this.location = location;
+    this.info = () => {
+        console.log(
+            `My name is ${this.firstName}, and I'm ${this.dateOfBirth} years old`
+        );
+    };
+};
+
+let userOne = new Users("Smith", 34, "Calicut");
+console.log(userOne);
+
+// EP: 43 / 65
+(function newDemo(a, b) {
+    console.log(a + b);
+})(2, 4);
+
+// EP: 44 / 65
+function init() {
+    let firstName = "John";
+    
+    function sayFistName() {
+        console.log(firstName);
+    }
+    sayFistName();
+}
+
+init();
+console.log(firstName);
+
+// EP: 45 / 65
+function init() {
+    let firstName = "John";
+    console.log("Demo");
+
+    function sayFistName() {
+        console.log(firstName);
+    }
+    return sayFistName;
+}
+
+let value = init();
+value();
+
+function doAdition(x) {
+    return function (y) {
+        return x + y;
+    };
+}
+
+let addItem = doAdition(6);
+console.log(addItem(9));
+console.log(doAdition(4)(4));
+
+// EP: 46 / 65
+
+function addFun(y) {
+    return function (w) {
+        return y - w;
+    };
+}
+console.log(addFun(100)(25));
+*/
+
+/*
+const userOne = {
+    num: 23,
+    newFun: function () {
+        console.log("T1:", this.num);
+        console.log("Demo");
+    },
+};
+
+const userTwo = {
+    num: 23,
+    newFun() {
+        console.log("T2:", this.num);
+        console.log("Demo");
+    },
+};
+
+userOne.newFun();
+userTwo.newFun();
+
+const users = {
+    fullName: "John Doe",
+    age: 34,
+    isLogin: true,
+    role: "admin",
+    getInfo() {
+        console.log(
+            `full name is ${this.fullName}, his role is ${this.role} and he is age ${this.age} older `
+        );
+    },
+};
+console.log(users);
+console.log(users.getInfo());
+
+const employee = {
+    fullName: "Wils Smith",
+    age: 45,
+    isLogin: false,
+    role: "user",
+};
+console.log(employee);
+
+let employeeInfo = users.getInfo.bind(employee);
+employeeInfo();
+
+console.log(users.getInfo());
+
+users.getInfo.call(employee);
+// EP: 48 / 65
+const courses = [
+    {
+        name: "Java Script",
+        price: 50,
+    },
+    {
+        name: "HTML",
+        price: 30,
+    },
+    {
+        name: "React JS",
+        price: 80,
+    },
+    {
+        name: "PHP",
+        price: 70,
+    },
+];
+
+function generateList() {
+    let ul = document.querySelector(".list");
+    ul.innerHTML = "";
+    courses.forEach((course) => {
+        let li = document.createElement("li");
+        li.className = "item";
+
+        let items = `
+            <span class="name">${course.name}</span>
+            <span class="price">$ ${course.price}</span>
+        `;
+        li.innerHTML = items;
+        ul.append(li);
+    });
+}
+
+window.addEventListener("load", generateList);
+
+let sorBtn = document.querySelector(".sort-button");
+sorBtn.addEventListener("click", () => {
+    courses.sort((a, b) => a.price - b.price);
+    generateList();
+});
+*/
+
+/*
+// EP: 51 / 65
+let myMap = new Map();
+
+myMap.set(1, "One");
+myMap.set(2, "two");
+myMap.set(3, "three");
+
+console.log(myMap);
+for (let key of myMap.keys()) {
+    console.log("key:", key);
+}
+for (let value of myMap.values()) {
+    console.log("value:", value);
+}
+
+// EP: 52 / 65
+const user = ["John", 23, "Admin"];
+//let [name, age, role] = user;
+
+//console.log("role:", role);
+
+const employeeList = {
+    name: "Smith",
+    age: 34,
+    role: "Super Admin",
+};
+let { name, age, role } = employeeList;
+console.log(age);
+
+// EP: 53 / 65
+function sumOne(a, b) {
+    return a + b;
+}
+
+console.log("SUM:", sumOne(4, 7));
+
+let numArr = [5, 7, 8];
+console.log("SP:", sumOne(...numArr)); // Spred OP
+
+// REST OP
+function sumTwo(a, b, ...args) {
+    console.log("a", args);
+    console.log("AB", a, b);
+    
+    let multy = a * b;
+    let sum = 0;
+    for (let arg of args) {
+        sum += arg;
+    }
+    
+    return [multy, sum];
+}
+console.log(sumTwo(2, 3, 5, 5, 5));
+
+// EP: 54 / 65 : Class
+class Users {
+    constructor(name, age, rank) {
+        this.name = name;
+        this.age = age;
+        this.rank = rank;
+    }
+    courseList = [];
+
+    getInfo() {
+        return { name: this.name, age: this.age, rank: this.rank };
+    }
+    endrollCourses(name) {
+        this.courseList.push(name);
+    }
+    getCourseList() {
+        return this.courseList;
+    }
+    login() {
+        return "User Login Success!";
+    }
+}
+
+let userOne = new Users("John", 34, 90);
+console.log("Info:", userOne.getInfo());
+
+userOne.endrollCourses("Java");
+userOne.endrollCourses("HTML");
+userOne.endrollCourses("CSS");
+console.log("Courses:", userOne.getCourseList());
+console.log(userOne.login());
+
+// EP: 56 / 65
+class SubAdmin extends Users {
+    constructor(name, age, rank) {
+        super(name, age, rank);
+    }
+    getAdminInfo() {
+        return "I'm Sub admin";
+    }
+    login() {
+        return "Sub Admmin Login Success!";
+    }
+}
+
+let adminOne = new SubAdmin("Wills", 25, 86);
+console.log(adminOne.getAdminInfo());
+console.log(adminOne.login());
+console.log(adminOne.getInfo());
+
+// EP: 57 / 65
+const funOne = () => {
+    console.log("Function One");
+};
+const funTwo = () => {
+    setTimeout(() => {
+        console.log("SET TIME OUT");
+    }, 3000);
+    console.log("Function Two");
+};
+const funThree = () => {
+    console.log("Function Three");
+};
+
+funOne();
+funThree();
+funTwo();
+
+// EP: 58 / 65
+const oneFun = () => {
+    return "First Fun";
+};
+const twoFun = () => {
+    return new Promise((sol, rej) => {
+        setTimeout(() => {
+            sol("Set Time");
+        }, 3000);
+    });
+};
+const threeFun = () => {
+    return "Therd Fun";
+};
+
+const getFun = async () => {
+    let one = oneFun();
+    console.log("A:", one);
+    
+    let two = await twoFun();
+    console.log("A:", two);
+    
+    let tree = threeFun();
+    console.log("A:", tree);
+};
+
+getFun();
+
+*/
+// EP: 59 / 65 - API
+fetch("https://api.chucknorris.io/jokes/random")
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log("DATA:", data)
+        let jok = data.value;
+        console.log("jok", jok);
+        console.log("data", data);
+    })
+    .catch();
